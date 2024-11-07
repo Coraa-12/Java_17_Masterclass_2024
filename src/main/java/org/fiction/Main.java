1f444523166e9e9a0bd57eac0e1c12c578d8a01e
Tegar Wijaya Kusuma <tegarwijayakusuma2004@gmail.com>
2024-11-08 00:15:07 +0700

    Add new class, main2

--- src/main/java/org/fiction/Main.java
+++ src/main/java/org/fiction/Main.java
@@ -2,7 +2,11 @@ package org.fiction;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello, World!");
+        System.out.println("Hello, World!");
         System.out.println("Hello, Java!");
+    }
+
+    public static void main2(String[] args) {
+        System.out.println("This is a new class");
     }
 }
