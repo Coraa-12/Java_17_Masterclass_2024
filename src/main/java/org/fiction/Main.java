77acc66bf97912816d26720e8de920ea24708f8f
Tegar Wijaya Kusuma <tegarwijayakusuma2004@gmail.com>
2024-11-10 16:07:35 +0700

    If statements

--- src/main/java/org/fiction/Main.java
+++ src/main/java/org/fiction/Main.java
@@ -2,6 +2,13 @@ package org.fiction;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Alice!");
+        boolean haveIdentityCard = false;
+        int age = 22;
+
+        if (haveIdentityCard && age >= 21) {
+            System.out.println("You are allowed to drink alcohol.");
+        } else {
+            System.out.println("You are not allowed to drink alcohol.");
+        }
     }
 }
--- src/main/java/org/fiction/Main2.java
+++ src/main/java/org/fiction/Main2.java
@@ -2,6 +2,12 @@ package org.fiction;
 
 public class Main2 {
     public static void main(String[] args) {
-        System.out.println("Hello, Me!");
+        boolean isAlice = true;
+
+        if (!isAlice) {
+            System.out.println("Hello, Alice!");
+        } else if (isAlice) {
+            System.out.println("Who are you?");
+        }
     }
 }
